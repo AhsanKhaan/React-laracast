aebadc73933f2862ce6fc2c1f69492b8e254def3
muhammad.ahsan <ahsankhan.ubit@gmail.com>
2022-07-02 09:58:47 -0700

    Development experience

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,28 @@
-import logo from './logo.svg';
-import { useState } from 'react';
-import './App.css';
-import React from 'react';
-import Another from './Another';
+import logo from "./logo.svg";
+import { useState } from "react";
+import "./App.css";
+import React from "react";
+import Another from "./Another";
 
 function App() {
   const someStyle = {
-    background : 'grey',
-    color      : 'white',
-    fontSize   : '28px',
-    fontWeight : 'bold' 
+    background: "grey",
+    color: "white",
+    fontSize: "28px",
+    fontWeight: "bold",
+    fontWeight: "as",
+  };
+  const [count, setCount] = useState(0);
+  function increment() {
+    setCount((prevCount) => prevCount + 1);
   }
-  const [count,setCount] = useState(0);
-  function increment(){
-  setCount(prevCount =>prevCount + 1);
+  function decrement() {
+    setCount((prevCount) => prevCount - 1);
   }
-  function decrement(){
-    setCount(prevCount=>prevCount - 1);
-    }
   return (
     <div className="App">
       <header className="App-header">
-      <Another name="John"/>
+        <Another name="John" />
         <div>
           <button onClick={decrement}>-</button>
           <span>{count}</span>
@@ -33,7 +34,7 @@ function App() {
         </p>
         <label htmlFor=""></label>
         {/* <p style={{background:'blue'}}>{ true && 2+3}</p> */}
-         <p style={someStyle}>{ true && 2+3}</p> 
+        <p style={someStyle}>{true && 2 + 3}</p>
         <a
           className="App-link"
           href="https://reactjs.org"
